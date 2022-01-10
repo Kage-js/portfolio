1893e6379e516f21b2dba4f12c6c1057e0dde019
ericfrancis313 <48592818+ericfrancis313@users.noreply.github.com>
2022-01-09 20:18:19 -0500

    updated resume

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
-const falling=document.querySelectorAll(".snowflake-start")
-const somethingElse=document.querySelector(".somethingElse")
-const enter=document.getElementById("enter-btn")
-const music= document.querySelector(".music")
-const webDev= document.querySelector(".webDev")
-const header = document.querySelector(".header")
-const pointer = document.querySelector(".pointer")
-const beforeBattle = document.querySelector(".beforeBattle")
-const beforeSpawn = document.querySelector(".beforeSpawn")
-const demonIntro = document.querySelector(".intro-demon")
-var delayInMilliseconds = 4000;
-
-
-falling.addEventListener("click",animationStart)
-enter.addEventListener("click",animationStart)
-somethingElse.addEventListener("click",animationStart)
-music.addEventListener("click",animationStart)
-webDev.addEventListener("click",animationStart)
-battle.addEventListener("click",animationStart)
-demonIntro.addEventListener("click",animationStart)
-function animationStart(){
-    event.preventDefault(); 
-    //for (var i = 0; i < falling.length; i++) {
-     //   falling[i].classList.add('snowflake');
-    //}
-    somethingElse.className="somethingEvil";
-    header.className="fadeUp"
-    webDev.className="fadeUp"    
-    music.className="fadeUp"
-    enter.className="fadeUp" 
-    pointer.className="fadeUp" 
-    setTimeout(function() {
-        window.location.assign("Home.html")
-           
-    }, 4000)
-    setTimeout(function() {
+function openMorningT(){
+    var text= document.getElementById("morningTText") 
+    var project=document.getElementById("morningT")
+    
+    if (text.style.display=="none"){
+        text.style.display ="inline"
+        project.style.alignSelf = "center"
+        project.style.backgroundColor = "hsl(49 37% 94%)"
+        project.style.color ="var(--bg-color)"
         
-    }, 4000)
+         
+    }else{
+        text.style.display ="none" 
+        project.style.textDecoration="none"
+        project.style.backgroundColor="var(--bg-color)"
+        project.style.color="hsl(49 37% 94%)"
+        
+        
+    }
+}
+
+function openAngularTask(){
+    var text= document.getElementById("angulartaskText") 
+    var project=document.getElementById("project angularTask")
+    if (text.style.display=="none"){
+        text.style.display ="inline"
+        project.style.backgroundColor = "hsl(49 37% 94%)"
+        project.style.color ="var(--bg-color)"
+
+    }else{
+        text.style.display ="none" 
+        project.style.textDecoration="none"
+        project.style.backgroundColor="var(--bg-color)"
+        project.style.color="hsl(49 37% 94%)"
+        
+    }
+}
+
+function openFgc(){
+    var text= document.getElementById("fgcText") 
+    var project=document.getElementById("project fgc")
+    if (text.style.display=="none"){
+        text.style.display ="inline"
+        project.style.backgroundColor = "hsl(49 37% 94%)"
+        project.style.color ="var(--bg-color)"
+
+    }else{
+        text.style.display ="none" 
+        project.style.textDecoration="none"
+        project.style.backgroundColor="var(--bg-color)"
+        project.style.color="hsl(49 37% 94%)"
+        
+    }
+}
+
+function openmfPyGame(){
+    var text= document.getElementById("mfPyGameText") 
+    var project=document.getElementById("project mfPyGame")
+    if (text.style.display=="none"){
+        text.style.display ="inline"
+        project.style.backgroundColor = "hsl(49 37% 94%)"
+        project.style.color ="var(--bg-color)"
+
+    }else{
+        text.style.display ="none" 
+        project.style.textDecoration="none"
+        project.style.backgroundColor="var(--bg-color)"
+        project.style.color="hsl(49 37% 94%)"
+        
+    }
 }
