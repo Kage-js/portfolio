e4d813810878521ad28ebf2da39680d55eb6a6f3
ericfrancis313 <48592818+ericfrancis313@users.noreply.github.com>
2022-01-10 16:10:29 -0500

    added twitch bot

--- index.js
+++ index.js
@@ -60,6 +60,25 @@ function openmfPyGame(){
         text.style.display ="inline"
         project.style.backgroundColor = "hsl(49 37% 94%)"
         project.style.color ="var(--bg-color)"
+        
+
+    }else{
+        text.style.display ="none" 
+        project.style.textDecoration="none"
+        project.style.backgroundColor="var(--bg-color)"
+        project.style.color="hsl(49 37% 94%)"
+        
+    }
+}
+
+function openChatBot(){
+    var text= document.getElementById("chatbotText") 
+    var project=document.getElementById("project chatbot")
+    if (text.style.display=="none"){
+        text.style.display ="inline"
+        project.style.backgroundColor = "hsl(49 37% 94%)"
+        project.style.color ="var(--bg-color)"
+        
 
     }else{
         text.style.display ="none" 
